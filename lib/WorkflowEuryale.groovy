3f243a99297c48f9390574b1ac5de1d5e77dee73
João Vitor <jvfecav@gmail.com>
2023-07-11 15:02:29 -0300

    refactor: Remove requirement for --genome param (#16)

--- lib/WorkflowEuryale.groovy
+++ lib/WorkflowEuryale.groovy
@@ -9,15 +9,7 @@ class WorkflowEuryale {
     //
     // Check and validate parameters
     //
-    public static void initialise(params, log) {
-        genomeExistsError(params, log)
-
-
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-            System.exit(1)
-        }
-    }
+    public static void initialise(params, log) {}
 
     //
     // Get workflow summary for MultiQC
@@ -61,17 +53,5 @@ class WorkflowEuryale {
         def description_html = engine.createTemplate(methods_text).make(meta)
 
         return description_html
-    }//
-    // Exit pipeline if incorrect --genome key provided
-    //
-    private static void genomeExistsError(params, log) {
-        if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
-            log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
-                "  Currently, the available genome keys are:\n" +
-                "  ${params.genomes.keySet().join(", ")}\n" +
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            System.exit(1)
-        }
     }
 }
